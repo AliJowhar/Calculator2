f587813c392eba55384a26b930d6d2276c4b139c
Ali Jowhar <alijowharcoding@gmail.com>
2023-12-05 20:13:21 -0600

    added functionality on number buttons

---
+++ main.js
@@ -0,0 +1,40 @@
+"using strint";
+
+const display = document.querySelector(".display-screen");
+
+const numBtn = document.querySelectorAll(".btn-num");
+const clearBtn = document.querySelector(".clear");
+
+// Mathmatical operations
+const add = function (a, b) {
+  return a + b;
+};
+
+const minus = function (a, b) {
+  return a - b;
+};
+const multiply = function (a, b) {
+  return a * b;
+};
+const divide = function (a, b) {
+  return a / b;
+};
+
+// Show what value the buttons have to the display
+const populateDisplay = function () {
+  numBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      //what is currently on the screen
+      const currentDisplayValue =
+        document.querySelector(".display-screen").textContent;
+
+      //what is on the screen plus the new button pressed in a variable
+      const newDisplay = currentDisplayValue + btn.textContent;
+
+      // display ^^ variable
+      display.textContent = newDisplay;
+    });
+  });
+};
+
+populateDisplay();
