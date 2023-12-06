5f35ec176e76f95666bdd84ca0b847309333528a
Ali Jowhar <alijowharcoding@gmail.com>
2023-12-06 16:04:37 -0600

    added more button functionality

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const title = document.querySelector(".title");
 
 const display = document.querySelector(".display-screen");
 
-const numBtn = document.querySelectorAll(".btn-num");
 const clearBtn = document.querySelector(".clear");
+const divideBtn = document.querySelector(".divide");
+const multiplyBtn = document.querySelector(".multiply");
+const minusBtn = document.querySelector(".minus");
+const addBtn = document.querySelector(".add");
+const equalsBtn = document.querySelector(".equals");
+const numBtn = document.querySelectorAll(".btn-num");
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let result = null;
 
 // Mathmatical operations
 const add = function (a, b) {
@@ -25,8 +31,11 @@ const divide = function (a, b) {
   return a / b;
 };
 
-const operation = function (a, b, operation) {
-  return operation(a, b);
+const operation = function (num1, num2, operator) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  return operator(num1, num2);
 };
 
 // Show what value the buttons have to the display
@@ -52,4 +61,81 @@ populateDisplay();
 // 1) functionality for operator buttons
 // 2) Reset button functionality
 // 3) Equals button equals button functionality
-// 4) Operator button fuctionality
+
+addBtn.addEventListener("click", () => {
+  if (firstNumber !== null) {
+    secondNumber = display.textContent;
+    operator = add;
+    result = operation(firstNumber, secondNumber, operator);
+    firstNumber = result;
+    console.log(`this is firstnum ${firstNumber}`);
+    console.log(` second num ${secondNumber}`);
+    console.log(`result ${result}`);
+    display.textContent = firstNumber;
+    secondNumber = null;
+  }
+  firstNumber = display.textContent;
+  operator = add;
+  display.textContent = "";
+});
+minusBtn.addEventListener("click", () => {
+  if (firstNumber !== null) {
+    secondNumber = display.textContent;
+    operator = minus;
+    result = operation(firstNumber, secondNumber, operator);
+    firstNumber = result;
+    console.log(`this is firstnum ${firstNumber}`);
+    console.log(` second num ${secondNumber}`);
+    console.log(`result ${result}`);
+    display.textContent = firstNumber;
+    secondNumber = null;
+  }
+  firstNumber = display.textContent;
+  operator = minus;
+  display.textContent = "";
+});
+multiplyBtn.addEventListener("click", () => {
+  if (firstNumber !== null) {
+    secondNumber = display.textContent;
+    operator = multiply;
+    result = operation(firstNumber, secondNumber, operator);
+    firstNumber = result;
+    console.log(`this is firstnum ${firstNumber}`);
+    console.log(` second num ${secondNumber}`);
+    console.log(`result ${result}`);
+    display.textContent = firstNumber;
+    secondNumber = null;
+  }
+  firstNumber = display.textContent;
+  operator = multiply;
+  display.textContent = "";
+});
+divideBtn.addEventListener("click", () => {
+  if (firstNumber !== null) {
+    secondNumber = display.textContent;
+    operator = divide;
+    result = operation(firstNumber, secondNumber, operator);
+    firstNumber = result;
+    console.log(`this is firstnum ${firstNumber}`);
+    console.log(` second num ${secondNumber}`);
+    console.log(`result ${result}`);
+    display.textContent = firstNumber;
+    secondNumber = null;
+  }
+  firstNumber = display.textContent;
+  operator = divide;
+  display.textContent = "";
+});
+
+equalsBtn.addEventListener("click", () => {
+  if (firstNumber !== null) {
+    secondNumber = display.textContent;
+    result = operation(firstNumber, secondNumber, operator);
+    display.textContent = "";
+    display.textContent = result;
+    firstNumber = result;
+
+    firstNumber = null;
+    secondNumber = null;
+  }
+});
